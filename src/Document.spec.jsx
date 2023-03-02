wojtekmaj/react-pdf

[breaking] Rename entry files to index files

src/Document.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { fireEvent, getByTestId, render } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.jest';
 
 import Document from './Document';
 import DocumentContext from './DocumentContext';

src/Outline.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.jest';
 
 import { OutlineInternal as Outline } from './Outline';
 

src/OutlineItem.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent, getAllByRole, render, screen } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.jest';
 import { OutlineItemInternal as OutlineItem } from './OutlineItem';
 
 import { loadPDF, makeAsyncCallback } from '../test-utils';

src/Page.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { fireEvent, render } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.jest';
 
 import { PageInternal as Page } from './Page';
 

src/Page/AnnotationLayer.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.jest';
 
 import { AnnotationLayerInternal as AnnotationLayer } from './AnnotationLayer';
 import LinkService from '../LinkService';

src/Page/PageCanvas.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.jest';
 
 import { PageCanvasInternal as PageCanvas } from './PageCanvas';
 

src/Page/PageSVG.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.jest';
 
 import { PageSVGInternal as PageSVG } from './PageSVG';
 

src/Page/TextLayer.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.jest';
 
 import { TextLayerInternal as TextLayer } from './TextLayer';
 

justin-midas/pdf-react

src/Document.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { fireEvent, getByTestId, render } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.test';
 
 import Document from './Document';
 import DocumentContext from './DocumentContext';

src/Outline.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.test';
 
 import { OutlineInternal as Outline } from './Outline';
 

src/OutlineItem.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent, getAllByRole, render, screen } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.test';
 import { OutlineItemInternal as OutlineItem } from './OutlineItem';
 
 import { loadPDF, makeAsyncCallback } from '../test-utils';

src/Page.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { fireEvent, render } from '@testing-library/react';
 
-import { pdfjs } from './entry.jest';
+import { pdfjs } from './index.test';
 
 import { PageInternal as Page } from './Page';
 

src/Page/AnnotationLayer.spec.jsx
@@ -1,7 +1,7 @@
 import React, { createRef } from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.test';
 
 import { AnnotationLayerInternal as AnnotationLayer } from './AnnotationLayer';
 import LinkService from '../LinkService';

src/Page/PageCanvas.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.test';
 
 import { PageCanvasInternal as PageCanvas } from './PageCanvas';
 

src/Page/PageSVG.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.test';
 
 import { PageSVGInternal as PageSVG } from './PageSVG';
 

src/Page/TextLayer.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import { pdfjs } from '../entry.jest';
+import { pdfjs } from '../index.test';
 
 import { TextLayerInternal as TextLayer } from './TextLayer';
 